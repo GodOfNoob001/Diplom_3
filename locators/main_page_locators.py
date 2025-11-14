1abe5d1f5cab5dc7e82d17f9e0f470cb1b828efe
Alex Dmiterko <dmiterko.sascha5@gmail.com>
2025-11-14 23:06:31 +0300

    В рамках данного проекта реализовано UI-автотестирование сервиса Stellar Burgers. При выполнении тестирования - были проверены основная функциональность и логика работы счётчиков заказов.

---
+++ locators/main_page_locators.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators:
+    CONSTRUCTOR_BUTTON = (By.XPATH, "//p[contains(@class, 'AppHeader_header__linkText__3q_va')][text()='Конструктор']")
+    ORDER_FEED_BUTTON = (By.XPATH, "//p[text()='Лента Заказов']")
+    INGREDIENT_MODAL = (By.XPATH, "//h2[text()='Детали ингредиента']")
+    SPECIAL_BUN = (By.XPATH, "//p[@class='BurgerIngredient_ingredient__text__yp3dH' and text()='Краторная булка N-200i']")
+    BUN_TOP_PLACEHOLDER = (By.XPATH, "//span[@class='constructor-element__text' and text()='Перетяните булочку сюда (верх)']")
+    SPECIAL_BUN_CONTAINER = (By.XPATH,"//a[contains(@href, 'ingredient') and .//img[@alt='Краторная булка N-200i']]")
+    CONSTRUCTOR_AREA = (By.XPATH, "//ul[@class='BurgerConstructor_basket__list__l9dp_']")
+    PLACE_ORDER_BUTTON = (By.XPATH, "//button[text()='Оформить заказ']")
+    LOGIN_BUTTON = (By.XPATH, "//button[text()='Войти в аккаунт']")
+    MODAL_ORDER_NUMBER = (By.CSS_SELECTOR, "h2.Modal_modal__title__2L34m")
+    MODAL_CLOSE_BUTTON = (By.CSS_SELECTOR, "button.Modal_modal__close_modified__3V5XS")
+    INGREDIENT_COUNTER = (By.CSS_SELECTOR, "p.counter_counter__num__3nue1")
